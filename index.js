6c2a71cde0e3178f6f160c1323ccba4b42b88a30
Your Name <you@example.com>
2018-12-08 18:22:34 +0400

    fs-i avelacum

--- index.js
+++ index.js
@@ -59,15 +59,90 @@
 
 
 
-var express = require("express");
-var app = express();
 
-app.use(express.static("public"));
 
-app.get("/", function(req, res){
-   res.redirect("index.html");
-});
+// var express = require("express");
+// var app = express();
 
-app.listen(3000, function(){
-   console.log("Example is running on port 3000");
-});
+// app.use(express.static("public"));
+
+// app.get("/", function(req, res){
+//    res.redirect("index.html");
+// });
+
+// app.listen(3000, function(){
+//    console.log("Example is running on port 3000");
+// });
+
+
+
+// var Square = require("./module");
+// var mySquareObject = new Square(5);
+
+// function main() {
+//    console.log(mySquareObject.getArea());
+// }
+// main();
+
+
+
+
+// var fs = require('fs');
+
+// function main(){
+//    fs.writeFile("hello.txt", "Hello world\n", function(err){
+//        console.log("fs.writeFile ended");
+//    });
+//    console.log("fs.writeFile");
+// }
+// main();
+
+
+
+
+// var fs = require('fs');
+
+// function main() {
+//    fs.writeFileSync("hello.txt", "Hello world\n");
+//    console.log("fs.writeFile");
+// }
+// main();
+
+
+
+
+var fs = require('fs');
+var obj = 
+{
+    "first_name": "Ani",
+    "last_name": "Gevorgyan",
+    "age": 15,
+    "tumo_student": true
+
+}
+
+var myJSON = JSON.stringify(obj);
+
+fs.writeFileSync("obj.json", myJSON);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+var obj =
+{
+    "first_name": "Ani",
+    "last_name": "Gevorgyan",
+    "age": 15,
+    "tumo_student": true
+
+}
